156527880810d60d0087bbd0e9ecfd5bec8ae573
realrnvr <156797453+realrnvr@users.noreply.github.com>
2026-09-17 01:59:14 +0530

    chore: add search implementation

--- src/main.rs
+++ src/main.rs
@@ -4,20 +4,18 @@ use std::fs;
 use std::process;
 
 use minigrep::search;
+use minigrep::search_case_insensitive;
 
 fn main() {
     let args: Vec<String> = env::args().collect();
 
     let config = Config::build(&args).unwrap_or_else(|err| {
-        println!("[ERROR]: Provide enough arguments: {err}", err = err);
+        eprintln!("[ERROR]: Provide enough arguments: {err}", err = err);
         process::exit(1);
     });
 
-    println!("[INFO]: Query: {query}", query = config.query);
-    println!("[INFO]: File : {file_path}", file_path = config.file_path);
-
     if let Err(err) = run(config) {
-        println!("[ERROR]: Failed to read the file: {err}", err = err);
+        eprintln!("[ERROR]: Failed to read the file: {err}", err = err);
         process::exit(1);
     }
 }
@@ -25,6 +23,7 @@ fn main() {
 struct Config<'a> {
     query: &'a str,
     file_path: &'a str,
+    ignore_case: bool,
 }
 
 impl<'a> Config<'a> {
@@ -38,18 +37,28 @@ impl<'a> Config<'a> {
 
         let query = &args[1];
         let file_path = &args[2];
+        let ignore_case = env::var("IGNORE_CASE").is_ok();
 
-        Ok(Config { query, file_path })
+        Ok(Config {
+            query,
+            file_path,
+            ignore_case,
+        })
     }
 }
 
 fn run(config: Config) -> Result<(), Box<dyn Error>> {
     let contents = fs::read_to_string(config.file_path)?;
 
-    for line in search(&config.query, &contents) {
+    let results = if config.ignore_case {
+        search_case_insensitive(config.query, &contents)
+    } else {
+        search(config.query, &contents)
+    };
+
+    for line in results {
         println!("{line}")
     }
 
-    println!("[INFO]: File contents:\n{contents}");
     Ok(())
 }
